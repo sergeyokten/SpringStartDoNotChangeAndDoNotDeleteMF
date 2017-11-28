dc1f12899b35c75876cbe1d82c074b9ea65b9093
sergeyokten <sergey.okten@gmail.com>
2017-11-28 17:51:38 +0200

    Initial commit

---
+++ src/main/java/ua/com/owu/entity/Customer.java
@@ -0,0 +1,27 @@
+package ua.com.owu.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String surname;
+
+}
